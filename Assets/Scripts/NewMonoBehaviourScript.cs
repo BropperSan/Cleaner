e096be825ef5683637dad18bf0fbed021ca4956c
BropperSan <115295833+BropperSan@users.noreply.github.com>
2025-10-27 21:46:28 +0300

    Hell Yeah

---
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            m_Rigidbody.AddForce(transform.forward * m_Thrust, ForceMode.Acceleration);
+        }
+    }
+}
