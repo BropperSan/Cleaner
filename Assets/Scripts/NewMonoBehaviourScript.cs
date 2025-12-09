28919204557ec43b0edc87cb14cd5601c36503b6
BropperSan <115295833+BropperSan@users.noreply.github.com>
2025-12-09 22:33:43 +0300

    Car

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -3,20 +3,79 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
-    public float m_Thrust = 20f;
+    public float motoreForce = 100f;
+    public float brakeForce = 1000f;
+    public float maxSteerAngle = 30f;
 
-    void Start()
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+
+    public Transform frontLeftWheelTransform;
+    public Transform rearLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+    public Transform rearRightWheelTransform;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteerAngle;
+    private float cureentBrakeForce;
+    private bool isBraking;
+
+
+    private void Update()
     {
-        //Fetch the Rigidbody from the GameObject with this script attached
-        m_Rigidbody = GetComponent<Rigidbody>();
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        isBraking = Input.GetKey(KeyCode.Space);
     }
 
-    void FixedUpdate()
+    private void HandleMotor()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            //Apply a force to this Rigidbody in direction of this GameObjects up axis
-            m_Rigidbody.AddForce(transform.forward * m_Thrust, ForceMode.Acceleration);
-        }
+        frontLeftWheelCollider.motorTorque = verticalInput * motoreForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motoreForce;
+
+        cureentBrakeForce = isBraking ? brakeForce : 0f;
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        frontLeftWheelCollider.brakeTorque = cureentBrakeForce;
+        frontRightWheelCollider.brakeTorque = cureentBrakeForce;
+        rearLeftWheelCollider.brakeTorque = cureentBrakeForce;
+        rearRightWheelCollider.brakeTorque = cureentBrakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
     }
 }
